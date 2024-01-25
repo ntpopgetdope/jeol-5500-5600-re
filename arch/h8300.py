88444a1b1e36320fa7a33b96e278941c79a88cd0
ntpopgetdope <17032481+offlineJ@users.noreply.github.com>
2024-01-25 05:01:55 +0000

    binja arch plugin changes
    begin defucking the architecture plugin, was originally targeted against the H8/300H 32-bit extension ISA, JEOLs are using 90s era original H8/338 ICs which only have the 8/16-bit addressing etc. instruction set fairly similar, defucking incrementally

--- arch/h8300.py
+++ arch/h8300.py
@@ -8,174 +8,134 @@ from types import *
 
 from .h8300dis import *
 from .h8300lift import InstructionIL 
-            
-# https://www.renesas.com/us/en/document/mah/h8300h-series-software-manual?language=en&r=1052456
 
 
-
-def decode_instruction(data, addr):
-        if len(data) < 2:
-            return None, None, None
-            
-        
-        inst, size, match = tryToParse(data) # in h8300dis
-        
-        if inst == None:
-            return None, None, None
-            
-        return inst, size, match
-        
-        
-def regFrom(reg, size):
-    reg_str = "???"
-    if size == 8:
-        if((reg&0x8) == 0):
-            reg_str = "r"+str(reg&7)+"h"
-        else:
-            reg_str = "r"+str(reg&7)+"l"
-    elif size == 16:
-        if((reg&0x8) == 0):
-            reg_str = "r"+str(reg&7)
-        else:
-            reg_str = "e"+str(reg&7)
-    elif size == 32:
-        reg_str = "er"+str(reg)
-    return reg_str
-    
-def signOffset(off, size):
-    if size == 8:
-        off = off & 0xFF
-        return off | (-(off & 0x80))
-    elif size == 16:
-        off = off & 0xFFFF
-        return off | (-(off & 0x8000))
-    elif size == 24:
-        off = off & 0xFFFFFF
-        return off | (-(off & 0x800000))
-    elif size == 32:
-        off = off & 0xFFFFFFFF
-        return off | (-(off & 0x80000000))
-    return off
+# https://api.binary.ninja/binaryninja.architecture-module.html
+# http://www.bitsavers.org/components/hitachi/h8/1990_H04_Hitachi_H8_330_HD6473308_HD643308_Hardware_Manual.pdf
+# https://github.com/ntpopgetdope/jeol-5500-5600-re/blob/main/doc/1998_R03_Hitachi_H8_336_337_338_HD6473388_Hardware_Manual.pdf
 
 class H8300(Architecture):
+    name = 'H8/300' # H8/338
 
-    name = 'H8/300'
-    
-    address_size = 3
+    address_size     = 2
     default_int_size = 1
-    instr_alignment = 1
+    instr_alignment  = 1
     max_instr_length = 10
-    
+
     endianness = Endianness.BigEndian
 
-    # register related stuff
-    regs = {
-    
-        # main registers
-        'er0': RegisterInfo('er0', 4),
-        'er1': RegisterInfo('er1', 4),
-        'er2': RegisterInfo('er2', 4),
-        'er3': RegisterInfo('er3', 4),
-        'er4': RegisterInfo('er4', 4),
-        'er5': RegisterInfo('er5', 4),
-        'er6': RegisterInfo('er6', 4),
-        'er7': RegisterInfo('er7', 4),
-        
-        # upper 16 
-        
-        'e0': RegisterInfo('er0', 2, 2),
-        'e1': RegisterInfo('er1', 2, 2),
-        'e2': RegisterInfo('er2', 2, 2),
-        'e3': RegisterInfo('er3', 2, 2),
-        'e4': RegisterInfo('er4', 2, 2),
-        'e5': RegisterInfo('er5', 2, 2),
-        'e6': RegisterInfo('er6', 2, 2),
-        'e7': RegisterInfo('er7', 2, 2),
-        
-        # lower 16 
-        
-        'r0': RegisterInfo('er0', 2, 0),
-        'r1': RegisterInfo('er1', 2, 0),
-        'r2': RegisterInfo('er2', 2, 0),
-        'r3': RegisterInfo('er3', 2, 0),
-        'r4': RegisterInfo('er4', 2, 0),
-        'r5': RegisterInfo('er5', 2, 0),
-        'r6': RegisterInfo('er6', 2, 0),
-        'r7': RegisterInfo('er7', 2, 0),
-        
-        # upper 8 of the lower 16
-        'r0h': RegisterInfo('er0', 1, 1),
-        'r1h': RegisterInfo('er1', 1, 1),
-        'r2h': RegisterInfo('er2', 1, 1),
-        'r3h': RegisterInfo('er3', 1, 1),
-        'r4h': RegisterInfo('er4', 1, 1),
-        'r5h': RegisterInfo('er5', 1, 1),
-        'r6h': RegisterInfo('er6', 1, 1),
-        'r7h': RegisterInfo('er7', 1, 1),
-        
-        
-        # lower 8 
-        'r0l': RegisterInfo('er0', 1, 0),
-        'r1l': RegisterInfo('er1', 1, 0),
-        'r2l': RegisterInfo('er2', 1, 0),
-        'r3l': RegisterInfo('er3', 1, 0),
-        'r4l': RegisterInfo('er4', 1, 0),
-        'r5l': RegisterInfo('er5', 1, 0),
-        'r6l': RegisterInfo('er6', 1, 0),
-        'r7l': RegisterInfo('er7', 1, 0),
-        
-        # program counter
-        'pc': RegisterInfo('pc', 4),
-        
-        # flag register
-        'ccr': RegisterInfo('ccr', 1),
+    '''
+    2.2.1 General Registers
 
+    All the general registers can be used as both data registers and address registers. When used as
+    address registers, the general registers are accessed as 16-bit registers (R0 to R7). When used as
+    data registers, they can be accessed as 16-bit registers, or the high and low bytes can be accessed
+    separately as 8-bit registers (R0H to R7H and R0L to R7L).
+    '''
+    regs = {
+        # -- General Registers --
+        'R0': RegisterInfo( 'R0', 2 ),
+        'R1': RegisterInfo( 'R1', 2 ),
+        'R2': RegisterInfo( 'R2', 2 ),
+        'R3': RegisterInfo( 'R3', 2 ),
+        'R4': RegisterInfo( 'R4', 2 ),
+        'R5': RegisterInfo( 'R5', 2 ),
+        'R6': RegisterInfo( 'R6', 2 ),
+        'R7': RegisterInfo( 'R7', 2 ),
+
+            # General Registers Lower 8-bits
+            'R0L': RegisterInfo( 'R0', 1, 0 ),
+            'R1L': RegisterInfo( 'R1', 1, 0 ),
+            'R2L': RegisterInfo( 'R2', 1, 0 ),
+            'R3L': RegisterInfo( 'R3', 1, 0 ),
+            'R4L': RegisterInfo( 'R4', 1, 0 ),
+            'R5L': RegisterInfo( 'R5', 1, 0 ),
+            'R6L': RegisterInfo( 'R6', 1, 0 ),
+            'R7L': RegisterInfo( 'R7', 1, 0 ),
+    
+            # General Registers Upper 8-bits
+            'R0H': RegisterInfo( 'R0', 1, 1 ),
+            'R1H': RegisterInfo( 'R1', 1, 1 ),
+            'R2H': RegisterInfo( 'R2', 1, 1 ),
+            'R3H': RegisterInfo( 'R3', 1, 1 ),
+            'R4H': RegisterInfo( 'R4', 1, 1 ),
+            'R5H': RegisterInfo( 'R5', 1, 1 ),
+            'R6H': RegisterInfo( 'R6', 1, 1 ),
+            'R7H': RegisterInfo( 'R7', 1, 1 ),
+
+        # -- Control Registers --
+        'PC' : RegisterInfo( 'PC',  2 ), # Program Counter
+        'CCR': RegisterInfo( 'CCR', 1 ), # Condition Code Register
     }
 
-    stack_pointer = "er7"
-    # Interrupt flag, User bit or interrupt mask bit
-    # Half-carry flag, Negative flag
-    # Zero flag, Overflow flag, Carry flag
-    flags = ['i', 'u', 'h', 'u2', 'n', 'z', 'v', 'c']
+    '''
+    2.2.1 General Registers
+
+    R7 also functions as the stack pointer, used implicitly
+    by hardware in processing interrupts & subroutine calls.
+    '''
+    stack_pointer = "R7"
+
+    # CCR reg:
+    flags = [
+        'I',  # Bit 7: Interrupt Mask Bit (I)
+        'U',  # Bit 6: User Bit           (U)
+        'H',  # Bit 5: Half-Carry Flag    (H)
+        'U2', # Bit 4: User Bit           (U)
+        'N',  # Bit 3: Negative Flag      (N)
+        'Z',  # Bit 2: Zero Flag          (Z)
+        'V',  # Bit 1: Overflow Flag      (V)
+        'C',  # Bit 0: Carry Flag         (C)
+    ]
 
     flag_roles = {
-        'i': FlagRole.SpecialFlagRole,
-        'u': FlagRole.SpecialFlagRole,
-        'u2': FlagRole.SpecialFlagRole,
-        
-        'n': FlagRole.NegativeSignFlagRole,
-        'z': FlagRole.ZeroFlagRole,
-        'h': FlagRole.HalfCarryFlagRole,
-        'v': FlagRole.OverflowFlagRole,
-        'c': FlagRole.CarryFlagRole
+        # Facilitates overriding handling
+        # in `get_flag_write_low_level_il`
+        'I' : FlagRole.SpecialFlagRole,
+        'U' : FlagRole.SpecialFlagRole,
+        'U2': FlagRole.SpecialFlagRole,
+
+        # Flags with binja defined behaviour
+        'H' : FlagRole.HalfCarryFlagRole,
+        'N' : FlagRole.NegativeSignFlagRole,
+        'Z' : FlagRole.ZeroFlagRole,
+        'V' : FlagRole.OverflowFlagRole,
+        'V' : FlagRole.CarryFlagRole
     }
     
-    flag_write_types = ['none', '*', 'c', 'z']
+    flag_write_types = [
+        'none',
+        '*',
+        'C',
+        'Z',
+    ]
+
     flags_written_by_flag_write_type = {
         'none': [],
-        '*': ['h', 'n', 'z', 'v', 'c'],
-        'c': ['c'],
-        'z': ['z'],
+        '*'   : ['H', 'N', 'Z', 'V', 'C'],
+        'C'   : ['C'],
+        'Z'   : ['Z'],
     }
     
+    # TODO:
     flags_required_for_flag_condition = {
-        LowLevelILFlagCondition.LLFC_UGT: ['c', 'z'], # hi
-        LowLevelILFlagCondition.LLFC_ULE: ['c', 'z'], # ls
-        LowLevelILFlagCondition.LLFC_UGE: ['c'], # cs
-        LowLevelILFlagCondition.LLFC_ULT: ['c'], # cs
-        LowLevelILFlagCondition.LLFC_NE:  ['z'], # ne
-        LowLevelILFlagCondition.LLFC_E:   ['z'], # eq
-        LowLevelILFlagCondition.LLFC_NO:  ['v'], # vc
-        LowLevelILFlagCondition.LLFC_O:   ['v'], # vs
-        LowLevelILFlagCondition.LLFC_POS: ['n'], # pl
-        LowLevelILFlagCondition.LLFC_NEG: ['n'], # mi
-        LowLevelILFlagCondition.LLFC_SGE: ['n', 'v'], # ge
-        LowLevelILFlagCondition.LLFC_SLT: ['n', 'v'], # lt
-        LowLevelILFlagCondition.LLFC_SGT: ['n', 'v', 'z'], # gt
-        LowLevelILFlagCondition.LLFC_SLE: ['n', 'v', 'z'], # le
+        LowLevelILFlagCondition.LLFC_UGT: ['C', 'Z'], # hi
+        LowLevelILFlagCondition.LLFC_ULE: ['C', 'Z'], # ls
+        LowLevelILFlagCondition.LLFC_UGE: ['C'], # cs
+        LowLevelILFlagCondition.LLFC_ULT: ['C'], # cs
+        LowLevelILFlagCondition.LLFC_NE:  ['Z'], # ne
+        LowLevelILFlagCondition.LLFC_E:   ['Z'], # eq
+        LowLevelILFlagCondition.LLFC_NO:  ['V'], # vc
+        LowLevelILFlagCondition.LLFC_O:   ['V'], # vs
+        LowLevelILFlagCondition.LLFC_POS: ['N'], # pl
+        LowLevelILFlagCondition.LLFC_NEG: ['N'], # mi
+        LowLevelILFlagCondition.LLFC_SGE: ['N', 'V'], # ge
+        LowLevelILFlagCondition.LLFC_SLT: ['N', 'V'], # lt
+        LowLevelILFlagCondition.LLFC_SGT: ['N', 'V', 'Z'], # gt
+        LowLevelILFlagCondition.LLFC_SLE: ['N', 'V', 'Z'], # le
     }
     
-    
+    # TODO: huh...
     """
     semantic_flag_classes = ['class_bitstuff']
 
@@ -212,10 +172,58 @@ class H8300(Architecture):
         return []    
     """
 
+    # Helper functions.
+    @staticmethod
+    def decode_instruction(data, addr):
+        if len(data) < 2:
+            return None, None, None
 
+
+        inst, size, match = tryToParse(data) # in h8300dis
+
+        if inst == None:
+            return None, None, None
+
+        return inst, size, match
+
+    @staticmethod
+    def regFrom(reg, size):
+        reg_str = "???"
+        if size == 8:
+            if((reg&0x8) == 0):
+                reg_str = "r"+str(reg&7)+"h"
+            else:
+                reg_str = "r"+str(reg&7)+"l"
+        elif size == 16:
+            if((reg&0x8) == 0):
+                reg_str = "r"+str(reg&7)
+            else:
+                reg_str = "e"+str(reg&7)
+        elif size == 32:
+            reg_str = "er"+str(reg)
+        return reg_str
+
+    @staticmethod
+    def signOffset(off, size):
+        if size == 8:
+            off = off & 0xFF
+            return off | (-(off & 0x80))
+        elif size == 16:
+            off = off & 0xFFFF
+            return off | (-(off & 0x8000))
+        elif size == 24:
+            off = off & 0xFFFFFF
+            return off | (-(off & 0x800000))
+        elif size == 32:
+            off = off & 0xFFFFFFFF
+            return off | (-(off & 0x80000000))
+        return off
+
+
+    # Binja architecture implementation.
     def get_instruction_info(self, data, addr):
     
-        inst, size, match = decode_instruction(data, addr)
+        inst, size, match = self.decode_instruction(data, addr)
         
         if inst == None:
             return None
@@ -244,7 +252,7 @@ class H8300(Architecture):
             # UNCONDITIONAL JUMP
             for l in inst[2]:
                 if l[0] == TYPE_PCOFFSET:
-                    result.add_branch(BranchType.UnconditionalBranch, addr + size + signOffset(match[l[2]], l[1]))
+                    result.add_branch(BranchType.UnconditionalBranch, addr + size + self.signOffset(match[l[2]], l[1]))
         
         
             
@@ -252,7 +260,7 @@ class H8300(Architecture):
             # CONDITIONAL BRANCH
             for l in inst[2]:
                 if l[0] == TYPE_PCOFFSET:
-                    result.add_branch(BranchType.TrueBranch, addr + size + signOffset(match[l[2]], l[1]))
+                    result.add_branch(BranchType.TrueBranch, addr + size + self.signOffset(match[l[2]], l[1]))
             result.add_branch(BranchType.FalseBranch, addr + size)
 
 
@@ -260,7 +268,7 @@ class H8300(Architecture):
             # DIRECT CALL
             for l in inst[2]:
                 if l[0] == TYPE_PCOFFSET:
-                    result.add_branch(BranchType.CallDestination, addr + size + signOffset(match[l[2]], l[1]))
+                    result.add_branch(BranchType.CallDestination, addr + size + self.signOffset(match[l[2]], l[1]))
             
         if instName == "JMP":
             if inst[2][0][0] == TYPE_ABS:
@@ -275,12 +283,8 @@ class H8300(Architecture):
             else:
                 # UNCONDITIONAL CALL TO REGISTER OR DOUBLE MEMORY LOOKUP
                 result.add_branch(BranchType.IndirectBranch)    
-            
-            
-            
+
         """
-
-
         if obj.name in ["CALL0", "CALL4", "CALL8", "CALL12"]:
             # DIRECT CALL
             for l in obj.prop["format"]:
@@ -291,13 +295,11 @@ class H8300(Architecture):
             # UNCONDITIONAL JUMP TO REGISTER
             result.add_branch(BranchType.IndirectBranch)    
         """
-            
         return result
-        
 
         
     def get_instruction_text(self, data, addr):
-        inst, size, match = decode_instruction(data, addr)
+        inst, size, match = self.decode_instruction(data, addr)
         
         if inst == None:
             return None
@@ -339,7 +341,7 @@ class H8300(Architecture):
                 result.append(InstructionTextToken(InstructionTextTokenType.EndMemoryOperandToken, ''))
             elif l[0] == TYPE_ATREG:
                 reg = match[l[4]]
-                regStr = regFrom(reg, l[1])
+                regStr = self.regFrom(reg, l[1])
                 
                 if l[3] == None or l[3] == '-' or l[3] == '+':
 
@@ -351,7 +353,7 @@ class H8300(Architecture):
                         result.append(InstructionTextToken(InstructionTextTokenType.TextToken, '+'))
                     result.append(InstructionTextToken(InstructionTextTokenType.EndMemoryOperandToken, ''))
                 else:
-                    offset = signOffset(match[l[3][2]], l[3][1])
+                    offset = self.signOffset(match[l[3][2]], l[3][1])
                     
                     result.append(InstructionTextToken(InstructionTextTokenType.BeginMemoryOperandToken, '@('))
                     result.append(InstructionTextToken(InstructionTextTokenType.IntegerToken, hex(offset), offset))
@@ -361,25 +363,24 @@ class H8300(Architecture):
    
             elif l[0] == TYPE_REG:
                 reg = match[l[2]]
-                regStr = regFrom(reg, l[1])
+                regStr = self.regFrom(reg, l[1])
                 result.append(InstructionTextToken(InstructionTextTokenType.RegisterToken, regStr))
             elif l[0] == TYPE_PCOFFSET:
-                pcOffset = signOffset(match[l[2]], l[1])
+                pcOffset = self.signOffset(match[l[2]], l[1])
                 result.append(InstructionTextToken(InstructionTextTokenType.PossibleAddressToken, hex(addr+pcOffset+size), addr+pcOffset+size))
             #    
             parIndex = parIndex + 1
             
         return result, size
-  
-    
+
+
     def get_flag_write_low_level_il(self, op, size, write_type, flag, operands, il):
         return Architecture.get_flag_write_low_level_il(self, op, size, write_type, flag, operands, il)
-        
-        
+
 
     def get_instruction_low_level_il(self, data, addr, il):
         if len(data) < 2: return None
-        inst, size, match = decode_instruction(data, addr)
+        inst, size, match = self.decode_instruction(data, addr)
         if inst == None: return None
 
         instName = inst[0].split(" ")[0]
@@ -398,7 +399,7 @@ class H8300(Architecture):
             elif l[0] == TYPE_ATABS:
                 args.append((TYPE_ATABS, math.ceil(l[1]/8), math.ceil(l[2]/8), match[l[3]]))
             elif l[0] == TYPE_ATREG:
-                r = regFrom(match[l[4]], l[1])
+                r = self.regFrom(match[l[4]], l[1])
                 sizeO = math.ceil(l[1]/8)
                 sizeA = math.ceil(l[2]/8)
                 if l[3] == None or l[3] == '-' or l[3] == '+':
@@ -409,11 +410,11 @@ class H8300(Architecture):
                     else:
                         args.append((TYPE_ATREG, sizeO, sizeA, "@", r))
                 else:
-                    args.append((TYPE_ATREG, sizeO, sizeA, "offset", r, signOffset(match[l[3][2]], l[3][1])))
+                    args.append((TYPE_ATREG, sizeO, sizeA, "offset", r, self.signOffset(match[l[3][2]], l[3][1])))
             elif l[0] == TYPE_REG:
-                args.append((TYPE_REG, math.ceil(l[1]/8), regFrom(match[l[2]], l[1])))
+                args.append((TYPE_REG, math.ceil(l[1]/8), self.regFrom(match[l[2]], l[1])))
             elif l[0] == TYPE_PCOFFSET:
-                args.append((TYPE_PCOFFSET, math.ceil(l[1]/8), addr+signOffset(match[l[2]], l[1])+size, addr+size))
+                args.append((TYPE_PCOFFSET, math.ceil(l[1]/8), addr+self.signOffset(match[l[2]], l[1])+size, addr+size))
         
         if InstructionIL.get(instName) is not None:
             instLifted = InstructionIL[instName](il, args)
